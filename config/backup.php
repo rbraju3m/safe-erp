2ca684b083e3f7e629bb613d92bd6c0a3d7c884c
rbraju3m <rbraju3m@gmail.com>
2025-11-20 01:21:51 +0600

    database backup with file

---
+++ config/backup.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+    'backup' => [
+
+        'name' => 'database-backup-safe',
+
+        'source' => [
+            'files' => [
+                'include' => [base_path()],
+                'exclude' => [base_path('vendor'), base_path('node_modules')],
+                'follow_links' => false,
+                'ignore_unreadable_directories' => false,
+                'relative_path' => null,
+            ],
+
+            'databases' => ['mysql'],
+        ],
+
+        'database_dump_compressor' => null,
+        'database_dump_file_extension' => '',
+
+        'destination' => [
+            'filename_prefix' => '',
+            'disks' => ['r2'],
+        ],
+
+        'temporary_directory' => storage_path('app/backup-temp'),
+
+        'password' => null,
+        'encryption' => 'default',
+    ],
+
+    'notifications' => [
+        'notifications' => [
+            \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => [],
+            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => [],
+            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => [],
+            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => [],
+            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => [],
+            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => [],
+        ],
+        'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
+    ],
+
+    'monitor_backups' => [
+        [
+            'name' => env('APP_NAME', 'laravel-backup'),
+            'disks' => ['r2'],
+            'health_checks' => [
+                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 7,
+                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+            ],
+        ],
+    ],
+
+    'cleanup' => [
+        'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
+        'default_strategy' => [
+            'keep_all_backups_for_days' => 0,
+            'keep_daily_backups_for_days' => 0,
+            'keep_weekly_backups_for_weeks' => 0,
+            'keep_monthly_backups_for_months' => 0,
+            'keep_yearly_backups_for_years' => 0,
+            'delete_oldest_backups_when_using_more_megabytes_than' => 1, // keeps only last 2 backups
+        ],
+    ],
+
+];
